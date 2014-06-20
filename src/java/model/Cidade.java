362882f41157164f5ee6cc55284e4351f64a1051
saulobittencourt <saulobittencourt_2@hotmail.com>
2014-06-19 22:11:49 -0300

    logando normal

--- src/java/model/Cidade.java
+++ src/java/model/Cidade.java
@@ -1,15 +1,11 @@
 package model;
 
 import java.io.Serializable;
-import java.util.Collection;
-import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import javax.persistence.OneToMany;
-import javax.xml.bind.annotation.XmlTransient;
 
 @Entity
 public class Cidade implements Serializable {
@@ -19,8 +15,6 @@ public class Cidade implements Serializable {
     @Column(name = "id_cidade")
     private Integer idCidade;
     private String cidade;
-    @OneToMany(cascade = CascadeType.ALL, mappedBy = "cidadeid")
-    private Collection<Item> itemCollection;
 
     public Cidade() {
     }
@@ -48,14 +42,5 @@ public class Cidade implements Serializable {
 
     public void setCidade(String cidade) {
         this.cidade = cidade;
-    }
-
-    @XmlTransient    
-    public Collection<Item> getItemCollection() {
-        return itemCollection;
-    }
-
-    public void setItemCollection(Collection<Item> itemCollection) {
-        this.itemCollection = itemCollection;
     }
 }
